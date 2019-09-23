f268bece80fc399f6c9bb42a2e9375234dbb0e90
xuliang <xuliang@efunong.com>
2019-09-23 11:22:38 +0800

    add

---
+++ src/main/java/com/example/onjava8demo/Onjava8demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.onjava8demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Onjava8demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Onjava8demoApplication.class, args);
+    }
+
+}
